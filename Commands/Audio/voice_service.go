fecc5686a060e21b4109d38a29d168746acc4e71
haydn <trolllemon@protonmail.com>
2024-10-12 09:55:58 -0700

    formatting and removed top comment

--- Commands/Audio/voice_service.go
+++ Commands/Audio/voice_service.go
@@ -1,28 +1,16 @@
 package audio
 
 import (
-
-
 	"github.com/bwmarrin/dgvoice"
 	"github.com/bwmarrin/discordgo"
 )
 
-
-
-/*  Dependency Injection Implementations
- * 
- *  We abstract the DiscordGO voice connection, and put 
- *  The audio playing, and voice connection stuff behind an interface.
- *  This makes testing easier to do, and also if we decide to switch the bot library
- *  we only need to change the interfaces implementation, not the audio players
- */
 type VoiceService interface {
 	PlayAudioFile(url string, Done chan bool)
 	Disconnect()
 }
 
-
-/* 
+/*
  * Voice service using DiscordGO and dgVoice
  */
 type Voice struct {
@@ -33,14 +21,11 @@ func (v *Voice) PlayAudioFile(url string, Done chan bool) {
 	dgvoice.PlayAudioFile(v.Vc, url, Done)
 }
 
-
 func (v *Voice) clean() {
-	v.Vc=nil
+	v.Vc = nil
 }
 
-func (v *Voice) Disconnect(){
+func (v *Voice) Disconnect() {
 	v.Vc.Disconnect()
 	v.clean()
 }
-
-
