f0fbf55ac15aef81bbbcd877b70e6af9101e50d7
haydn <trolllemon@protonmail.com>
2024-11-07 15:30:48 -0800

    added image to bytes helper, and switched to http exceptions in read_HTTP

--- ImageManipulation/util.py
+++ ImageManipulation/util.py
@@ -1,8 +1,8 @@
 import cv2 as cv
 import numpy as np
 import requests
-
-
+import io
+from fastapi import HTTPException
 
 def read_HTTP_into_mat(url : str):
     """
@@ -15,14 +15,27 @@ def read_HTTP_into_mat(url : str):
     :return: np.ndarray, numpy aray image
     """    
     if ".gif" in url:
-        raise ValueError("Gifs not supported")
-
+        raise HTTPException(status_code=415, detail=f".gif files are not supported")
     try: 
         response = requests.get(url, stream=True).raw
-        image = np.asarray(bytearray(response.read()), dtype=np.uint8)
-        image = cv.imdecode(image, cv.IMREAD_COLOR)
-        if image is None:
-            raise ValueError("Could not decode the image from the URL, likely doesn't point to an image file")
-        return image
     except requests.exceptions.RequestException as e:
-        raise ValueError(f"Error fetching image: {e}")
+        raise HTTPException(status_code=422, detail=f"given url doesn't point to an image: {e}")
+    image = np.asarray(bytearray(response.read()), dtype=np.uint8)
+    image = cv.imdecode(image, cv.IMREAD_COLOR)
+    if image is None:
+         raise HTTPException(status_code=500, detail=f"failed to decode image")
+    return image
+
+
+def image_to_bytes(image : np.ndarray) -> io.BytesIO:
+     """
+     image_to_bytes
+     encodes a numpy image into bytes in the png format
+     :param image: numpy array, our image, in any format
+     """
+     _, buffer = cv.imencode('.png', image)
+     image_bytes = io.BytesIO(buffer)
+     image_bytes.seek(0)
+     return image_bytes
+
+
