c142b2351a0ccfc741104d5a3f3d6173ed0e88e7
haydn <trolllemon@protonmail.com>
2024-11-08 16:21:42 -0800

    random filter

--- ImageManipulation/manip/__init__.py
+++ ImageManipulation/manip/__init__.py
@@ -2,4 +2,5 @@ from .colors import Invert, Saturate
 from .edges import edgeDetect
 from .morphology import Dilate, Erode
 from .quality import reduce
-from .text import add_text 
+from .text import add_text
+from .random import randomFilter
--- ImageManipulation/server.py
+++ ImageManipulation/server.py
@@ -1,4 +1,4 @@
-from manip import Invert, Saturate, edgeDetect, Dilate, Erode, reduce, add_text
+from manip import Invert, Saturate, edgeDetect, Dilate, Erode, reduce, add_text, randomFilter
 from util import read_HTTP_into_mat, image_to_bytes
 from fastapi import FastAPI, HTTPException
 
@@ -10,13 +10,24 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+@app.get("/api/randomFilteredImage/{image_link:path}/{kernel_size}/{low}/{high}/{kernel_type}")
+async def filter_image_random(image_link: str, kernel_size: int, low: int, high: int, kernel_type: str):
+    image = read_HTTP_into_mat(image_link)
 
+    if kernel_size <= 0:
+        raise HTTPException(status_code=400, detail=f'Kernel size must be greater than 0, got {kernel_size}')
 
+    should_norm = kernel_type == "norm"
+
+    random_filtered_image = randomFilter(image,kernel_size,low,high, normalize=should_norm)
+    image_bytes = image_to_bytes(random_filtered_image)
+
+    return StreamingResponse(image_bytes, media_type="image/png")
 
 @app.get("/api/invertedImage/{image_link:path}")
 async def invert_image(image_link: str):
     image = read_HTTP_into_mat(image_link)
-    
+
     inverted_image = Invert(image)
     image_bytes = image_to_bytes(inverted_image)
 
@@ -26,7 +37,7 @@ async def invert_image(image_link: str):
 @app.get("/api/saturatedImage/{image_link:path}/{saturation}")
 async def saturate_image(image_link: str, saturation: float):
     image = read_HTTP_into_mat(image_link)
-    
+
     saturated_image = Saturate(image,saturation)
     image_bytes = image_to_bytes(saturated_image)
 
@@ -35,10 +46,10 @@ async def saturate_image(image_link: str, saturation: float):
 @app.get("/api/edgeImage/{image_link:path}/{lower}/{higher}")
 async def edge_detect_image(image_link: str, lower: int, higher: int):
     image = read_HTTP_into_mat(image_link)
-    
+
     if lower <= 0 or higher <= 0:
-        raise HTTPException(status_code=400, detail=f'lower and higher bounds and iterations must be greater than 0, got {lower} and {higher}') 
-    
+        raise HTTPException(status_code=400, detail=f'lower and higher bounds and iterations must be greater than 0, got {lower} and {higher}')
+
     edges = edgeDetect(image,lower,higher)
     image_bytes = image_to_bytes(edges)
 
@@ -48,12 +59,12 @@ async def edge_detect_image(image_link: str, lower: int, higher: int):
 
 @app.get("/api/dilatedImage/{image_link:path}/{box_size}/{iterations}")
 async def dilate_image(image_link:str, box_size: int, iterations: int):
-    
+
     image = read_HTTP_into_mat(image_link)
-    
+
     if box_size <= 0 or iterations <= 0:
-        raise HTTPException(status_code=400, detail=f'box size and iterations must be greater than 0, got {box_size} and {iterations}') 
-    
+        raise HTTPException(status_code=400, detail=f'box size and iterations must be greater than 0, got {box_size} and {iterations}')
+
     dilated_image = Dilate(image, kernel_size=box_size, iterations=iterations)
     image_bytes = image_to_bytes(dilated_image)
 
@@ -63,10 +74,10 @@ async def dilate_image(image_link:str, box_size: int, iterations: int):
 async def erode_image(image_link:str, box_size: int, iterations: int):
 
     image = read_HTTP_into_mat(image_link)
-    
+
     if box_size <= 0 or iterations <= 0:
-        raise HTTPException(status_code=400, detail=f'box size and iterations must be greater than 0, got {box_size} and {iterations}') 
-    
+        raise HTTPException(status_code=400, detail=f'box size and iterations must be greater than 0, got {box_size} and {iterations}')
+
     eroded_image = Erode(image, kernel_size=box_size, iterations=iterations)
     image_bytes = image_to_bytes(eroded_image)
 
@@ -80,7 +91,7 @@ async def add_text_to_image(image_link:str, text:str, location:str):
 
 @app.get("/api/reducedImage/{image_link:path}/{quality}")
 async def reduce_image(image_link: str, quality: float):
-    
+
     image = read_HTTP_into_mat(image_link)
     if quality <= 0.0 or quality > 1.0:
         raise HTTPException(status_code=400, detail=f'quality level must be between 0 and 1, got {quality}')
