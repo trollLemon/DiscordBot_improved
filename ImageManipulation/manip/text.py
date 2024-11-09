5b10df86c1e02642c8d126e8b7c381e910b9b920
haydn <trolllemon@protonmail.com>
2024-11-08 18:40:05 -0800

    add text implementation

---
+++ ImageManipulation/manip/text.py
@@ -0,0 +1,24 @@
+import cv2 as cv
+import numpy as np
+
+def add_text(image : np.ndarray, text, font_scale:float, x_perc:float ,y_perc:float) -> np.ndarray:
+    
+    font = cv.FONT_HERSHEY_COMPLEX
+    foreground = (255,255,255)
+    outline    = (0,0,0)
+    rows, cols,_ = image.shape
+    x, y = rows * x_perc, cols * y_perc
+    print(x,y)
+    foreground_thickness = 2 
+    outline_thickness = 4
+
+    image_with_text = cv.putText(image,text,(int(x),int(y)),font,font_scale, outline, outline_thickness, cv.LINE_AA, False)
+    image_with_text = cv.putText(image_with_text,text,(int(x),int(y)),font,font_scale, foreground, foreground_thickness, cv.LINE_AA, False)
+
+    return image_with_text
+
+
+
+
+
+
