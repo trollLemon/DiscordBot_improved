e1ff7ef6842a4723e27076db86d4f2c9ceb3f9b9
haydn <trolllemon@protonmail.com>
2024-12-31 20:26:21 -0800

    moved to microservices dir

--- ImageManipulation/manip/__init__.py
+++
@@ -1,6 +0,0 @@
-from .colors import Invert, Saturate
-from .edges import EdgeDetect
-from .morphology import Dilate, Erode
-from .quality import Reduce
-from .text import Add_text
-from .random import RandomFilter
