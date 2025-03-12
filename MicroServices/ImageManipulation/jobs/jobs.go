27c6485e2c03eebd27fb05af39fc3152757df906
haydn <trolllemon@protonmail.com>
2025-03-11 20:22:17 -0700

    struct embed for operation

---
+++ MicroServices/ImageManipulation/jobs/jobs.go
@@ -0,0 +1,51 @@
+package jobs
+
+import (
+	"gocv.io/x/gocv"
+)
+
+type Operation interface {
+	Run(input gocv.Mat, output gocv.Mat) error
+}
+
+
+
+
+/*
+Job Struct
+
+*/
+type Job struct {
+	jobId       uint8
+	Operation
+	inputImage  gocv.Mat
+	outputImage gocv.Mat
+}
+
+func (j *Job) Process() error {
+	return j.Run(j.inputImage, j.outputImage)
+}
+
+func (j *Job) GetResult() gocv.Mat {
+	return j.outputImage
+}
+
+func (j *Job) GetJobId() uint8 {
+	return j.jobId
+}
+
+type Invert struct{}
+
+func (i Invert) Run(input gocv.Mat, output gocv.Mat) error {
+	return nil
+}
+
+
+type EdgeDetect struct {
+	t_lower  float64
+	t_higher float64
+}
+
+func (e EdgeDetect) Run(input gocv.Mat, output gocv.Mat) error {
+	return nil
+}
