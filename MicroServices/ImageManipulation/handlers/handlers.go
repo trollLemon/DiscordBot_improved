ba4420848185438fbb457e2167386fa41f9b5b08
haydn <trolllemon@protonmail.com>
2025-04-22 14:05:24 -0700

    encode to png before sending bytes

---
+++ MicroServices/ImageManipulation/handlers/handlers.go
@@ -0,0 +1,73 @@
+package handlers
+
+import (
+	"context"
+	"errors"
+	"github.com/rs/zerolog/log"
+	"gocv.io/x/gocv"
+	"image_manip/jobs"
+	"time"
+)
+
+var (
+	jobId = 0
+)
+
+func getNewJobId() uint8 {
+	return uint8(jobId + 1)
+}
+
+func ProcessImage(job *jobs.Job, jobRequests chan *jobs.JobRequest, ctx context.Context) ([]byte, error) {
+	jobRequest := jobs.NewJobRequest(job)
+	resultChan := jobRequest.Result
+	errChan := jobRequest.Error
+	jobRequests <- jobRequest
+
+	//defer cancel()
+	defer close(resultChan)
+	defer close(errChan)
+
+	select {
+	case result := <-resultChan:
+		{
+			buf, err := gocv.IMEncode(".png", *result)
+			if err != nil {
+				log.Err(err).Msg("failed to encode result into png format")
+				return nil, err
+			}
+			return buf.GetBytes(), nil
+		}
+	case err := <-errChan:
+		{
+			return nil, err
+		}
+
+	case <-ctx.Done():
+		return nil, errors.New("job cancelled due to timeout")
+
+	}
+
+}
+
+func InvertImage(jobRequests chan *jobs.JobRequest, image *gocv.Mat) ([]byte, error) {
+	job := jobs.NewJob(getNewJobId(), jobs.NewInvert(), image)
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	return ProcessImage(job, jobRequests, ctx)
+
+}
+
+func SaturateImage(jobRequests chan *jobs.JobRequest, image *gocv.Mat, value float32) ([]byte, error) {
+	job := jobs.NewJob(getNewJobId(), jobs.NewSaturate(value), image)
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	return ProcessImage(job, jobRequests, ctx)
+}
+
+func DetectEdges(jobRequests chan *jobs.JobRequest, image *gocv.Mat, tLower, tHigher float32) ([]byte, error) {
+	job := jobs.NewJob(getNewJobId(), jobs.NewEdgeDetection(tLower, tHigher), image)
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	return ProcessImage(job, jobRequests, ctx)
+
+}
---
+++ MicroServices/ImageManipulation/handlers/handlers_test.go
@@ -0,0 +1,85 @@
+package handlers_test
+
+import (
+	"context"
+	"errors"
+	"gocv.io/x/gocv"
+	"image_manip/handlers"
+	"image_manip/jobs"
+	"image_manip/worker"
+	"testing"
+	"time"
+)
+
+type MockOperationSuccess struct {
+}
+type MockOperationErr struct {
+}
+
+type MockOperationTimeOut struct{}
+
+func (m MockOperationTimeOut) Run(input *gocv.Mat) (*gocv.Mat, error) {
+
+	time.Sleep(time.Second * 15)
+	return nil, nil
+}
+
+func (m MockOperationErr) Run(input *gocv.Mat) (*gocv.Mat, error) {
+	return nil, errors.New("error processing job")
+}
+
+func (m MockOperationSuccess) Run(input *gocv.Mat) (*gocv.Mat, error) {
+	return input, nil
+}
+
+func TestInvertImageHandler(t *testing.T) {
+
+	requests := make(chan *jobs.JobRequest)
+	testImage := gocv.NewMatWithSize(1920, 1080, gocv.MatTypeCV32F)
+	testCtx, cancel := context.WithTimeout(context.Background(), time.Second*5)
+	defer cancel()
+	tests := []struct {
+		name        string
+		wantErr     bool
+		wantTimeout bool
+		job         *jobs.Job
+		ctx         context.Context
+	}{
+		{
+			name:        "Test success",
+			wantErr:     false,
+			wantTimeout: false,
+			job:         jobs.NewJob(0, MockOperationSuccess{}, &testImage),
+		},
+		{
+			name:        "Test Failure",
+			wantErr:     true,
+			wantTimeout: false,
+			job:         jobs.NewJob(0, MockOperationErr{}, &testImage),
+		},
+		{
+			name:        "Test Timeout",
+			wantErr:     true,
+			wantTimeout: false,
+			job:         jobs.NewJob(0, MockOperationTimeOut{}, &testImage),
+		},
+	}
+
+	go worker.Worker(0, requests)
+
+	timeOutError := errors.New("job cancelled due to timeout")
+
+	for _, tt := range tests {
+
+		_, err := handlers.ProcessImage(tt.job, requests, testCtx)
+
+		if tt.wantTimeout && !errors.Is(err, timeOutError) {
+			t.Errorf("TestInvertImageHandler() error = %v, wantErr %v, expected %s", err, tt.wantErr, timeOutError.Error())
+		}
+
+		if (err != nil) != tt.wantErr {
+			t.Errorf("TestInvertImageHandler() Test %s, error = %v, wantErr %v", tt.name, err, tt.wantErr)
+		}
+
+	}
+}
