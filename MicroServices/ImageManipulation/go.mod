794d7ec5523b5396f3c65f3a923b85d4e4aa1b5d
haydn <trolllemon@protonmail.com>
2025-04-22 18:32:36 -0700

    new testing package

--- MicroServices/ImageManipulation/go.mod
+++ MicroServices/ImageManipulation/go.mod
@@ -5,17 +5,18 @@ go 1.24.0
 require (
 	github.com/labstack/echo/v4 v4.13.3
 	github.com/rs/zerolog v1.34.0
+	go.uber.org/goleak v1.3.0
 	gocv.io/x/gocv v0.40.0
 )
 
 require (
 	github.com/labstack/gommon v0.4.2 // indirect
-	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-colorable v0.1.14 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.2 // indirect
-	golang.org/x/crypto v0.31.0 // indirect
-	golang.org/x/net v0.33.0 // indirect
-	golang.org/x/sys v0.28.0 // indirect
-	golang.org/x/text v0.21.0 // indirect
+	golang.org/x/crypto v0.36.0 // indirect
+	golang.org/x/net v0.37.0 // indirect
+	golang.org/x/sys v0.31.0 // indirect
+	golang.org/x/text v0.23.0 // indirect
 )
--- MicroServices/ImageManipulation/main.go
+++ MicroServices/ImageManipulation/main.go
@@ -1,30 +1,48 @@
-
 /*
-    - /api/randomFilteredImage/{image_link:path}/{kernel_size}/{low}/{high}/{kernel_type}
-    - /api/invertedImage/{image_link:path}
-    - /api/saturatedImage/{image_link:path}/{saturation}
-    - /api/edgeImage/{image_link:path}/{lower}/{higher}
-    - /api/dilatedImage/{image_link:path}/{box_size}/{iterations}
-    - /api/erodedImage/{image_link:path}/{box_size}/{iterations}
-    - /api/textImage/{image_link:path}/{text}/{font_scale}/{x}/{y}
-    - /api/reducedImage/{image_link:path}/{quality}
-    - /api/shuffledImage/{image_link:path}/{partitions}
+   - /api/randomFilteredImage/{image_link:path}/{kernel_size}/{low}/{high}/{kernel_type}
+   - /api/invertedImage/{image_link:path}
+   - /api/saturatedImage/{image_link:path}/{saturation}
+   - /api/edgeImage/{image_link:path}/{lower}/{higher}
+   - /api/dilatedImage/{image_link:path}/{box_size}/{iterations}
+   - /api/erodedImage/{image_link:path}/{box_size}/{iterations}
+   - /api/textImage/{image_link:path}/{text}/{font_scale}/{x}/{y}
+   - /api/reducedImage/{image_link:path}/{quality}
+   - /api/shuffledImage/{image_link:path}/{partitions}
 */
 
-package imagemanipulation
-
-
-
+package main
 
 import (
 	"github.com/labstack/echo/v4"
+	"github.com/rs/zerolog/log"
+	"image_manip/jobs"
+	"image_manip/worker"
+	"net/http"
 )
 
+func initRouting(e *echo.Echo) {
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!")
+	})
 
-func init_routing(e  *echo.Echo){}
+	e.Logger.Fatal(e.Start(":1323"))
 
-func main(){
-e := echo.New()
+}
 
-init_routing(e)
+func GraceFullShutdown(jobReqs chan *jobs.JobRequest) {
+	log.Info().Msg("Closing worker request channels")
+	close(jobReqs)
+
+}
+
+func main() {
+	e := echo.New()
+	initRouting(e)
+	numWorkers := 4
+	jobReqs := make(chan *jobs.JobRequest, numWorkers)
+
+	for workerId := range numWorkers {
+		go worker.Worker(workerId, jobReqs)
+	}
+
 }
