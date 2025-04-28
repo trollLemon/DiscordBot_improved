48b188aaf02244bcc77049946a026ab5a6c890ad
haydn <trolllemon@protonmail.com>
2025-04-27 18:58:07 -0700

    removed in favor of gomanip service

--- MicroServices/ImageManipulationPython/manip/__init__.py
+++
@@ -1,7 +0,0 @@
-from .colors import Invert, Saturate
-from .edges import EdgeDetect
-from .morphology import Dilate, Erode
-from .quality import Reduce
-from .text import AddText
-from .random import RandomFilter
-from .misc import Shuffle
