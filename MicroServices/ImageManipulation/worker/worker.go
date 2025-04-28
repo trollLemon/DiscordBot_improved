cfacea74ac2bbc0d311de216ce9b8913a063ee7c
haydn <trolllemon@protonmail.com>
2025-04-27 18:50:31 -0700

    add case for program shutdown and timeout cancelation

--- MicroServices/ImageManipulation/worker/worker.go
+++ MicroServices/ImageManipulation/worker/worker.go
@@ -3,36 +3,47 @@ package worker
 import (
 	"context"
 	"github.com/rs/zerolog/log"
-	"image_manip/jobs"
+	"goManip/jobs"
 	"sync"
 )
 
-func Worker(ctx context.Context, workerId int, jobRequests <-chan *jobs.JobRequest, wg *sync.WaitGroup) {
+func Worker(shutdown context.Context, workerId int, jobRequests <-chan *jobs.JobRequest, wg *sync.WaitGroup) {
 	defer wg.Done()
 
 	for jobRequest := range jobRequests {
 
 		job := jobRequest.Job
-
 		log.Info().Msgf("Worker %d: Starting job: %d", workerId, job.GetJobId())
-
 		result, err := job.Process()
 		select {
 
-		case <-ctx.Done():
-			log.Info().Msgf("Worker %d: shutdown", workerId)
+		case <-shutdown.Done():
+			log.Info().Int("Worker", workerId).Msg("Worker shutdown")
 			return
 
+		// if the job timed out, no goroutine is waiting for a result.
+		// we don't need to send anything
+		case <-jobRequest.Ctx.Done():
+			result.Close()
+
 		default:
-			if err == nil {
-				log.Info().Msgf("Worker %d: Finished job: %d in %d ns", workerId, job.GetJobId(), job.GetTimeElapsed())
-				jobRequest.Result <- result
-				close(jobRequest.Result)
+
+			if err != nil {
+				log.Error().
+					Str("Start time", job.GetStartTime().String()).
+					Str("End time", job.GetEndTime().String()).
+					Msgf("Worker %d Failed: %s", workerId, err.Error())
 			} else {
-				log.Error().Msgf("Worker %d: Error processing job %d: %v. Job started at %d and failed at %d", workerId, job.GetJobId(), err.Error(), job.GetStartTime().UnixMilli(), job.GetEndTime().UnixMilli())
-				jobRequest.Error <- err
-				close(jobRequest.Error)
+				log.Info().
+					Str("Start time", job.GetStartTime().String()).
+					Str("End time", job.GetEndTime().String()).
+					Int("Duration (ns)", job.GetTimeElapsed()).
+					Msgf("Worker %d Completed", workerId)
 			}
+
+			jobRequest.Result <- &jobs.Result{Image: result, Error: err}
+
 		}
+
 	}
 }
