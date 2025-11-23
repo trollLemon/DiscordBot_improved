36739515a94e3cc80c567a604c16c9f6cd1536f5
trollLemon <trolllemon@protonmail.com>
2025-11-23 01:22:20 +0000

    http error function package

---
+++ services/GoManip/errors/errors.go
@@ -0,0 +1,33 @@
+package errors
+
+import (
+	"strconv"
+
+	"github.com/labstack/echo/v4"
+)
+
+
+type ErrorType int 
+
+
+type GomanipError struct {
+	Status string `json:"status"`
+	Detail string `json:"detail"`
+}
+
+
+// ReturnJsonError returns a json payload containing the error message and status code.
+func ReturnJsonError(c echo.Context, statusCode int, errString string) error {
+	response := &GomanipError{
+		Status: strconv.Itoa(statusCode),
+		Detail: errString,
+	}
+
+	return c.JSON(statusCode, response)
+}   
+
+
+
+
+
+
