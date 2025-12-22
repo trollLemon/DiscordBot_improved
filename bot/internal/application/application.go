af758a2f1887971ca4dbd6439b96bcae4ff34d22
trolllemon <trolllemon@protonmail.com>
2025-12-22 23:56:48 +0000

    application initialization in application package

--- bot/internal/application/application.go
+++ bot/internal/application/application.go
@@ -1,9 +1,15 @@
 package application
 
 import (
+
+	"context"
+
+	"github.com/redis/go-redis/v9"
+
 	"github.com/trollLemon/DiscordBot/internal/classification"
 	"github.com/trollLemon/DiscordBot/internal/gomanip"
 	"github.com/trollLemon/DiscordBot/internal/randomwords"
+	"github.com/trollLemon/DiscordBot/internal/common"
 )
 
 type Application struct {
@@ -11,4 +17,28 @@ type Application struct {
 	Classification *Classification.ImageClassification
 	RandomWords    *randomwords.RandomWords
 	GuildID        string
+}
+
+
+
+func InitializeApplication(conf *common.Config, ctx context.Context) *Application {
+
+	gomanip := gomanip.NewGoManip(conf.GomanipURL, conf.GomanipTimeout)
+
+	classifier := Classification.NewImageClassification(conf.ClassificationTimeout, conf.ClassificationURL, Classification.SendImageEndpoint, Classification.GetClassificationEndpoint)
+
+	redisClient := redis.NewClient(&redis.Options{
+		Addr:     conf.DbDSN,
+		Password: conf.RedisPass,
+		DB:       conf.RedisSetNumber,
+	})
+
+	randomWordsStore := randomwords.NewRandomWords(redisClient, ctx, conf.RandomWordsSetName)
+
+
+	return &Application{
+		Gomanip:        gomanip,
+		Classification: classifier,
+		RandomWords:    randomWordsStore,
+	}
 }
