e055704eaede686b1f3655143862d3b351e6fa56
trolllemon <trolllemon@protonmail.com>
2025-12-22 23:56:02 +0000

    cleaned up main

--- bot/cmd/main.go
+++ bot/cmd/main.go
@@ -5,19 +5,14 @@ import (
 	"flag"
 	"os"
 	"os/signal"
-	"strconv"
-	"time"
 
 	"github.com/bwmarrin/discordgo"
-	"github.com/redis/go-redis/v9"
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
 
-	application "github.com/trollLemon/DiscordBot/internal/application"
-	"github.com/trollLemon/DiscordBot/internal/classification"
+	"github.com/trollLemon/DiscordBot/internal/application"
 	"github.com/trollLemon/DiscordBot/internal/commands"
-	"github.com/trollLemon/DiscordBot/internal/gomanip"
-	"github.com/trollLemon/DiscordBot/internal/randomwords"
+	"github.com/trollLemon/DiscordBot/internal/common"
 )
 
 type Options struct {
@@ -25,44 +20,7 @@ type Options struct {
 	PrettyPrint      bool
 }
 
-type Config struct {
-	BotToken  string
-	RedisPass string
 
-	GomanipURL        string
-	ClassificationURL string
-	DbDSN             string
-
-	GomanipTimeout        time.Duration
-	ClassificationTimeout time.Duration
-
-	RandomWordsSetName string
-	RedisSetNumber     int
-}
-
-func registerCommands(session *discordgo.Session) {
-	registeredCommands := make([]*discordgo.ApplicationCommand, len(Commands.SlashCommands))
-	gid := os.Getenv("GUILD_ID")
-	for i, v := range Commands.SlashCommands {
-		cmd, err := session.ApplicationCommandCreate(session.State.User.ID, gid, v)
-		if err != nil {
-			log.Panic().Msgf("Cannot create '%v' command: %v", v.Name, err)
-		}
-		registeredCommands[i] = cmd
-		log.Printf("Registered Command %v", v.Name)
-	}
-}
-
-func addCommandHandlers(session *discordgo.Session, app *application.Application) {
-	session.AddHandler(func(s *discordgo.Session, i *discordgo.InteractionCreate) {
-		if h, ok := Commands.CommandHandlers[i.ApplicationCommandData().Name]; ok {
-			if err := h(s, i, app); err != nil {
-				log.Error().Err(err).Msg("Failed to execute command")
-			}
-
-		}
-	})
-}
 
 func parseCommandLineArgs() *Options {
 
@@ -74,64 +32,6 @@ func parseCommandLineArgs() *Options {
 		*shouldRegisterCommands,
 		*prettyPrint,
 	}
-
-}
-
-func InitializeApplication(conf *Config, ctx context.Context) *application.Application {
-
-	gomanip := gomanip.NewGoManip(conf.GomanipURL, conf.GomanipTimeout)
-
-	classifier := Classification.NewImageClassification(conf.ClassificationTimeout, conf.ClassificationURL, Classification.SendImageEndpoint, Classification.GetClassificationEndpoint)
-
-	redisClient := redis.NewClient(&redis.Options{
-		Addr:     conf.DbDSN,
-		Password: conf.RedisPass,
-		DB:       conf.RedisSetNumber,
-	})
-
-	randomWordsStore := randomwords.NewRandomWords(redisClient, ctx, conf.RandomWordsSetName)
-
-
-	return &application.Application{
-		Gomanip:        gomanip,
-		Classification: classifier,
-		RandomWords:    randomWordsStore,
-	}
-}
-
-func configure() *Config {
-	conf := Config{
-		BotToken:           os.Getenv("DISCORD_TOKEN"),
-		RedisPass:          os.Getenv("REDIS_PASS"),
-		GomanipURL:         os.Getenv("GOMANIP_URL"),
-		ClassificationURL:  os.Getenv("CLASSIFICATION_URL"),
-		DbDSN:              os.Getenv("DATABASE_DSN"),
-		RandomWordsSetName: os.Getenv("RANDOM_WORD_SET_NAME"),
-	}
-
-	gomanipTimeout, err := time.ParseDuration(os.Getenv("GOMANIP_TIMEOUT"))
-	if err != nil {
-		log.Error().Msgf("failed to parse duration in provided variable GOMANIP_TIMEOUT=%s. Using default value of 30 seconds.", os.Getenv("GOMANIP_TIMEOUT"))
-		gomanipTimeout = time.Second * 30
-	}
-
-	classificationTimeout, err := time.ParseDuration(os.Getenv("CLASSIFICATION_TIMEOUT"))
-	if err != nil {
-		log.Error().Msgf("failed to parse duration in provided variable CLASSIFICATION_TIMEOUT=%s. Using default value of 5 minutes.", os.Getenv("CLASSIFICATION_TIMEOUT"))
-		gomanipTimeout = time.Minute * 5
-	}
-
-	conf.GomanipTimeout = gomanipTimeout
-	conf.ClassificationTimeout = classificationTimeout
-
-	setNum, err := strconv.Atoi(os.Getenv("REDIS_SET_NUM"))
-	if err != nil {
-		log.Error().Msgf("failed to parse string in provided variable REDIS_SET_NUM=%s. Using default value of 0.", os.Getenv("REDIS_SET_NUM"))
-	}
-
-	conf.RedisSetNumber = setNum
-
-	return &conf
 }
 
 func main() {
@@ -143,7 +43,7 @@ func main() {
 
 	log.Info().Msg("Reading config from ENV")
 
-	conf := configure()
+	conf := common.GetBotConfig()
 
 	session, err := discordgo.New("Bot " + conf.BotToken)
 	if err != nil {
@@ -162,14 +62,14 @@ func main() {
 	if options.RegisterCommands {
 		go func() {
 		    log.Info().Msg("Registering commands...")
-		    registerCommands(session)
+		    commands.RegisterCommands(session)
 		}()
 	}
 
 	log.Info().Msg("Initializing application")
-	app := InitializeApplication(conf, context.Background())
+	app := application.InitializeApplication(conf, context.Background())
 
-	addCommandHandlers(session, app)
+	commands.AddCommandHandlers(session, app)
 
 	log.Info().Msg("Bot is online.")
 	defer session.Close()
