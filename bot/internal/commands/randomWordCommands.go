64b102548213af504e284612a418540e537f4e8e
trolllemon <trolllemon@protonmail.com>
2025-12-22 00:12:25 +0000

    ../common/replies.go
    use new messaging functions

--- bot/internal/commands/randomWordCommands.go
+++ bot/internal/commands/randomWordCommands.go
@@ -14,29 +14,29 @@ func Add(s *discordgo.Session, i *discordgo.InteractionCreate, a *application.Ap
 	term := i.ApplicationCommandData().Options[0].StringValue()
 
 	if err := a.RandomWords.Insert(term); err != nil {
-		Common.Reply(s, i, fmt.Sprintf("error inserting word: %s. %s", term, err.Error()))
+		Common.RandomWordsError(s, i, "Insert Failed", term, err)
 		return err
 	}
 
-	Common.Reply(s, i, fmt.Sprintf("Added %s to database", term))
+	Common.Reply(s, i, fmt.Sprintf("Added %s to the word list.", term))
 	return nil
 }
 func Remove(s *discordgo.Session, i *discordgo.InteractionCreate, a *application.Application) error {
 	term := i.ApplicationCommandData().Options[0].StringValue()
 
 	if err := a.RandomWords.Delete(term); err != nil {
-		Common.Reply(s, i, fmt.Sprintf("error removing word: %s. %s", term, err.Error()))
+		Common.RandomWordsError(s, i, "Removed Failed", term, err)
 		return err
 	}
 
-	Common.Reply(s, i, fmt.Sprintf("Added %s to database", term))
+	Common.Reply(s, i, fmt.Sprintf("Removed %s from the word list.", term))
 	return nil
 }
 func Show(s *discordgo.Session, i *discordgo.InteractionCreate, a *application.Application) error {
 	terms, err := a.RandomWords.GetAll()
 
 	if err != nil {
-		Common.Reply(s, i, "error getting all words in database")
+		Common.Reply(s, i, fmt.Sprintf("error removing word: %s. %s", "", err.Error()))
 		return err
 	}
 	wordsString := strings.Join(terms, "\n")
