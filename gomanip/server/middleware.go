2d0cf622b22e84c5e96d4b6c068cbe8c5d7f2ce8
trolllemon <trolllemon@protonmail.com>
2025-12-31 23:42:19 +0000

    formatting

--- gomanip/server/middleware.go
+++ gomanip/server/middleware.go
@@ -1,10 +1,9 @@
 package server
 
-
 import (
+	"errors"
 	"fmt"
 	"slices"
-	"errors"
 
 	"github.com/labstack/echo/v4"
 	"github.com/rs/zerolog/log"
@@ -13,10 +12,9 @@ import (
 )
 
 var (
-	supportedFileTypes = []string{ "image/png", "image/jpeg"}
+	supportedFileTypes = []string{"image/png", "image/jpeg"}
 	ErrUnsupportedFile = errors.New("invalid filetype")
 )
-
 
 func JobDispatcherMiddleware(jobDispatcher *jobdispatch.JobDispatcher) echo.MiddlewareFunc {
 	return func(next echo.HandlerFunc) echo.HandlerFunc {
@@ -28,9 +26,9 @@ func JobDispatcherMiddleware(jobDispatcher *jobdispatch.JobDispatcher) echo.Midd
 }
 
 func FileTypeVerifyMiddleware() echo.MiddlewareFunc {
-	return func (next echo.HandlerFunc) echo.HandlerFunc  {
+	return func(next echo.HandlerFunc) echo.HandlerFunc {
 		return func(c echo.Context) error {
-			contentType:= c.Request().Header.Get("Content-Type")
+			contentType := c.Request().Header.Get("Content-Type")
 
 			if !slices.Contains(supportedFileTypes, contentType) {
 				log.Error().Msg(fmt.Sprintf("request had content type of %s which is not supported", contentType))
